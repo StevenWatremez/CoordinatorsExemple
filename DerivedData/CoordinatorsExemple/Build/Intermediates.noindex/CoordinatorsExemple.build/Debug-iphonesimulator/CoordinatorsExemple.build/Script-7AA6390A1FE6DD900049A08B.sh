f5bf944f1fc12308c88be0bafc32d452eddf6de5
Steven Watremez <steven.watremez@niji.fr>
2017-12-17 23:20:24 +0100

    gitignore

--- DerivedData/CoordinatorsExemple/Build/Intermediates.noindex/CoordinatorsExemple.build/Debug-iphonesimulator/CoordinatorsExemple.build/Script-7AA6390A1FE6DD900049A08B.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-if which swiftgen >/dev/null; then
-  SRCDIR="$PROJECT_DIR/$PROJECT_NAME"
-  swiftgen storyboards -t swift3 "$SRCDIR" --output "$SRCDIR/Constants/Storyboards.swift"
-else
-  echo "warning: SwiftGen not installed, download it from https://github.com/AliSoftware/SwiftGen"
-fi
